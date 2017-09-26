8bb7e28c1fd734ffa8c77d58ba1cee9746ea4540
Du YueHan <yhdu35@foxmail.com>
2017-09-27 00:03:44 +0800

    what: login route done

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,18 +2,33 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import './stylus/index.styl'
 import Vue from 'vue'
-import {router} from './router'
+import VueRouter from 'vue-router'
+import routes from './router/index'
 import MessageBox from 'vue-msgbox'
-import App from './App'
+import App from './App.vue'
 import store from './vuex/store'
 
 Vue.config.productionTip = false
-/**
- * router的beforeEach操作之所以放在main里面,而不是把所有与router相关的都放在router.js里面
- * 是因为涉及到循环引用的问题,router引了store,store引了router,
- * 循环引用的话会出现的情况参见:http://es6.ruanyifeng.com/#docs/module#循环加载
- * 导致store在login组件拿到的是个空对象,所以就让router.js提前执行完毕,把router.beforeEach放这了.
- * */
+
+Vue.use(VueRouter)
+const router = new VueRouter(routes)
+
+router.beforeEach((to, from, next) => {
+  // login页
+  if (to.name === 'login') {
+    next()
+  } else {
+    if (store) {
+      if (store.state.token.token !== null) {
+        next()
+      } else {
+        next({path: '/login'})
+      }
+    } else {
+      next({path: '/login'})
+    }
+  }
+})
 
 window.alert = MessageBox
 
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,19 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
 import LoginView from 'components/Login/Login.vue'
 import PostsView from 'components/Posts/Posts.vue'
 
 // import store from '../vuex/store'
 
-Vue.use(VueRouter)
-const router = new VueRouter({
-  routes: [
-    {path: '/login', component: LoginView},
-    {path: '/posts', component: PostsView},
-    {path: '*', redirect: '/posts'}
-  ]
-})
+const routes = [
+  {path: '/login', component: LoginView, name: 'login'},
+  {path: '/posts', component: PostsView},
+  {path: '*', redirect: '/posts'}
+]
 
-export {
-  router
+export default {
+  routes
 }
