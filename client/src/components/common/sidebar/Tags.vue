d916096f909f0468adc05b70f411d7abb09ef5af
wolfdu <yhdu35@foxmail.com>
2017-10-25 18:57:33 +0800

    what: tags todo

---
+++ client/src/components/common/sidebar/Tags.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-contain">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data () {
+      return {
+        article: {}
+      }
+    },
+    beforeRouteEnter (to, from, next) {
+    },
+    mounted () {
+      this.$nextTick(function () {
+      })
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="stylus">
+  .sidebar
+    -webkit-box-flex 1
+    -ms-flex 1 1 33.4%
+    flex 1 1 33.4%
+    padding-left 21.333px
+    padding-left 2rem
+</style>
