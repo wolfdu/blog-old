3cc80a4403248cc9d32a83c4705d0bf01039d1d8
Du YueHan <yhdu35@foxmail.com>
2017-12-05 20:05:49 +0800

    what: :rocket: modify blog scroll top

--- client/src/router.js
+++ client/src/router.js
@@ -10,7 +10,7 @@ const About = () => import('components/About.vue')
 Vue.use(Router)
 
 const routes = [
-  {path: '/posts', component: PostsList, meta: {goTop: true}},
+  {path: '/posts', component: PostsList},
   {path: '/post', component: Post, name: 'post', meta: {goTop: true}},
   {path: '/tag', component: Tag, name: 'tag', meta: {goTop: true}},
   {path: '/about', component: About, name: 'about', meta: {goTop: true}},
@@ -22,10 +22,9 @@ const router = new Router({
   routes
 })
 
-router.beforeEach((to, from, next) => {
+router.afterEach((to, from, next) => {
   if (to.matched.some(record => record.meta.goTop)) {
     window.scroll(0, 0)
   }
-  next()
 })
 export default router
