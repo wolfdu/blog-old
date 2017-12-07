27b2909fd78d764bd9179a7ed3d8bfa9b07895a7
Du YueHan <yhdu35@foxmail.com>
2017-12-07 23:47:11 +0800

    what: :rocket: dashboard layout

--- admin/src/components/home/components/Dashboard.vue
+++ admin/src/components/home/components/Dashboard.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
-    <h3 class="page-title">疯狂的Dashboard</h3>
     <div class="dashboard-contain">
       <div class="main">
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
+        <div class="item header"></div>
+        <div class="item author"></div>
+        <div class="item other"></div>
+        <div class="item to-do-list"></div>
+        <div class="item link"></div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -28,9 +27,23 @@
     box-shadow 0 0 1px rgba(0,0,0,0.25)
     margin 10px
     .main
+      height 100%
+      width 100%
       display grid
-      grid-template-columns 1fr 6fr 1fr
+      grid-template-columns 1fr 1fr 1fr
       grid-gap 1rem
     .item
-      background lightblue
+      background lightsalmon
+    .header
+      grid-column 1/4
+      height 100px
+    .author
+      height 300px
+    .other
+      grid-column 2/4
+      grid-row 2/5
+    .to-do-list
+      height 300px
+    .link
+      height 300px
 </style>
